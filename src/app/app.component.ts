385f5e971248595a92b4a7b7f1743d923692cd25
abuashraf73 <32354150+abuashraf73@users.noreply.github.com>
2019-07-09 14:35:25 +0600

    .

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'HTTPget-postProject';
+
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com'; 
+  
+  posts: Observable<any[]>;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  getPosts(){
+    this.posts = this.http.get(this.ROOT_URL + '/posts');
+  }
+
+}
+ 
